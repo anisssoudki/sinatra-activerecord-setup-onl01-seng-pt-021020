ac7a0091719b5810f6317d50bb60934c0b262b46
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-17 22:48:27 +0000

    Automatically backed up by Learn

--- db/migrate/20200417224431_create_dogs.rb
+++ db/migrate/20200417224431_create_dogs.rb
@@ -9,6 +9,13 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   def down
     drop_table :dogs
   end
+  
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
 
 end
 
